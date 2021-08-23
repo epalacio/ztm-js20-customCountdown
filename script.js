cc0981d15503dfdf2df12e36c17cd7b8e71ef3be
Esteban Palacio <estipalacio@gmail.com>
2021-08-23 15:59:22 -0400

    added form values through js

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const inputContainer = document.getElementById('input-container')
 const countdownForm = document.getElementById('countdownForm')
 const dateEl = document.getElementById('date-picker')
 
+let countdownTitle = ''
+let countdownDate = ''
+
 // Set Date Input Min with today's date
 const today = new Date().toISOString().split('T')[0]
 dateEl.setAttribute('min', today)
+
+// Take values from Form Input
+const updateCountdown = (e) => {
+    e.preventDefault()
+    countdownTitle = e.srcElement[0].value
+    countdownDate = e.srcElement[1].value
+    console.log(countdownTitle, countdownDate);
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown)
